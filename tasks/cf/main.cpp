693292400b8b6b46a75e2d9044761c72527658fa
Nasedkin Dmitry <jamesnasedkin@gmail.com>
2024-12-05 12:30:12 +0300

    cf optimaze

--- tasks/cf/main.cpp
+++ tasks/cf/main.cpp
@@ -55,65 +55,106 @@ mt19937 rnd(static_cast<unsigned int>(chrono::steady_clock().now().time_since_ep
 #define setpr(_x) cout << setprecision(_x) << fixed
 #define debug(x) cout << __FUNCTION__ << ": " << #x " = " << (x) << endl
 
-vi operator *(vi a, vi b) {
-    vi res(sz(a));
-    for (int i = 0; i < sz(a); i++) {
-        res[i] = a[b[i]];
+const int N = 100;
+int red_buttons = 0;
+
+tuple<vector<int>, vector<int>, map<int, int>> decode_input(string &s, int k) {
+    vector<int> positions_of_santas;
+    vector<int> positions_of_box;
+    map<int, int> count_of_boxes_in_square;
+
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == '0') continue;
+        if (s[i] == '1') {
+            positions_of_box.push_back(i);
+            count_of_boxes_in_square[i]++;
+        } else if (s[i] == '2') {
+            positions_of_box.push_back(i);
+            count_of_boxes_in_square[i]++;
+        } else if (s[i] == '3'){
+            red_buttons++;
+            positions_of_box.push_back(i);
+            count_of_boxes_in_square[i]++;
+        } else if (s[i] == 'd') {
+            positions_of_santas.push_back(i);
+        } else if (s[i] == 'D') {
+            positions_of_santas.push_back(i);
+            count_of_boxes_in_square[i]++;
+        }
     }
-    return res;
+
+    return make_tuple(positions_of_santas, positions_of_box, count_of_boxes_in_square);
 }
 
-vi pow(vi a, int n) {
-    vi res = a;
-    for (int i = 1; i < n; i++) {
-        res = res * a;
+void ask(vector<pair<int, int>> ded) {
+    string result(N, 'N');
+    for (int i = 0; i < ded.size(); i++) {
+        if (ded[i].second == 0) {
+            result[ded[i].first] = 'S';
+        } else if (ded[i].second == 1) {
+            result[ded[i].first] = 'R';
+        } else if (ded[i].second == -1) {
+            result[ded[i].first] = 'L';
+        } else {
+            assert(false);
+        }
     }
-    return res;
+    cout << result << endl;
 }
 
-vi rev(vi a) {
-    vi res(sz(a));
-    for (int i = 0; i < sz(a); i++) {
-        res[a[i]] = i;
+void send_pos(vi &pos) {
+    for (int x : pos) {
+        cout << x;
     }
-    return res;
+    cout << endl;
 }
 
-bool check(vi delta, vi p1, vi p2) {
-    vi left = delta * p1 * delta;
-    vi right = p2;
-    return left == right;
+void first_pos(int k) {
+    vector<int> perm(100);
+    int dist = 100 / k;
+    int left = k;
+    for (int i = 0; i < 100; i++) {
+        if (i % dist == 0 && left > 0) {
+            left--;
+            perm[i] = 1;
+        }
+    }
+    send_pos(perm);
 }
+
+vector<pair<int, int>> fun(tuple<vector<int>, vector<int>, map<int, int>> input) {
+    auto positions_of_santas = get<0>(input);
+    auto positions_of_box = get<1>(input);
+    auto count_of_boxes_in_square = get<2>(input);
+
+    vector<pair<int, int>> ans;
+    if (red_buttons == 0) {
+        for (int x : positions_of_santas) {
+            ans.push_back({x, 0});
+        }
+    } else {
+        for (int x : positions_of_santas) {
+            ans.push_back({x, 1});
+        }
+    }
+    return ans;
+}
+
 
 void solve() {
-    vi p1 = {7, 2, 6, 4, 1, 3, 5, 0};
-    vi p2 = {6, 4, 0, 5, 2, 7, 1, 3};
-    vi p3 = {3, 7, 0, 5, 2, 1, 6, 4};
-    p2 = rev(p2);
-    p3 = pow(p3, 15);
-    p2 = p2 * p3;
-    p2 = pow(p2, 166);
-
-    vi ans(8);
-    iota(all(ans), 0);
-
-    do {
-        if (check(ans, p1, p2)) {
-            for (int i = 0; i < 8; i++) {
-                cout << ans[i] + 1 << ' ';
-            }
-            cout << '\n';
-        }
-    } while (next_permutation(all(ans)));
+    int k;
+    cin >> k;
+    first_pos(k);
+    while (red_buttons < 10000) {
+        cin >> red_buttons;
+        if (red_buttons >= 10000) break;
+        string s;
+        cin >> s;
+        ask(fun(decode_input(s, k)));
+    }
 }
 
 int main() {
     fast_input;
-
-#ifdef __APPLE__
-    freopen("in.txt", "r", stdin);
-    freopen("out.txt", "w", stdout);
-#endif
-
     solve();
 }
