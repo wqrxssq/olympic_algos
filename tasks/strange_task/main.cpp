1acfe4a67f92f00a2d43bde2beda9717cdeee108
Nasedkin Dmitry <jamesnasedkin@gmail.com>
2025-01-03 20:30:28 +0300

    task strange_task

---
+++ tasks/strange_task/main.cpp
@@ -0,0 +1,96 @@
+#pragma GCC optimize("O3,unroll-loops")
+
+#include <math.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cstring>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <climits>
+using namespace std;
+
+using uint = unsigned int;
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vpii = vector<pii>;
+const double PI = acos(-1);
+const double EPS = 1e-6;
+const int INF = 1e9;
+const ll INFLL = 1e18;
+const int MOD1 = 1e9 + 7;
+const int MOD2 = 1e9 + 9;
+
+mt19937 rnd(static_cast<unsigned int>(chrono::steady_clock().now().time_since_epoch().count()));
+
+#define all(_x) _x.begin(), _x.end()
+#define rall(_x) _x.rbegin(), _x.rend()
+#define pb push_back
+#define ff first
+#define ss second
+#define sz(_x) (int)_x.size()
+#define display(_x)        \
+    for (auto el : _x)     \
+        cout << el << ' '; \
+    cout << '\n'
+#define cin_arr(_x)     \
+    for (auto& el : _x) \
+        cin >> el;
+#define fast_input ios_base::sync_with_stdio(0); cin.tie(0)
+#define setpr(_x) cout << setprecision(_x) << fixed
+#define debug(x) cout << __FUNCTION__ << ": " << #x " = " << (x) << endl
+
+const int K = 26;
+
+struct Node {
+    Node* to[K] = {0};
+};
+
+const int MAXN = 3e5;
+int n;
+Node* root = new Node();
+Node* where[MAXN + 1];
+
+void solve() {
+    cin >> n;
+    where[0] = root;
+    int ans = 0;
+    for (int v = 1; v <= n; v++) {
+        int p;
+        char c;
+        cin >> p >> c;
+        p--;
+        if (!where[p]->to[c - 'a']) {
+            ans++;
+            where[p]->to[c - 'a'] = new Node();
+        }
+        where[v] = where[p]->to[c - 'a'];
+        cout << ans << '\n';
+    }
+}
+
+int main() {
+    fast_input;
+#ifdef __APPLE__
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+    solve();
+}
