0b95e2eb2222664e1b7724e361065e838693938c
Nasedkin Dmitry <jamesnasedkin@gmail.com>
2025-01-09 00:51:21 +0300

    new task glacer_travel input

---
+++ tasks/scanline/ glacier_travel/main.cpp
@@ -0,0 +1,82 @@
+#pragma GCC optimize("O3,unroll-loops")
+
+#include <math.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cstring>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <climits>
+using namespace std;
+
+using uint = unsigned int;
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vpii = vector<pii>;
+const double PI = acos(-1);
+const double EPS = 1e-6;
+const int INF = 1e9;
+const ll INFLL = 1e18;
+const int MOD = 1e9 + 7;
+
+mt19937 rnd(static_cast<unsigned int>(chrono::steady_clock().now().time_since_epoch().count()));
+
+#define all(_x) _x.begin(), _x.end()
+#define rall(_x) _x.rbegin(), _x.rend()
+#define pb push_back
+#define ff first
+#define ss second
+#define sz(_x) (int)_x.size()
+#define display(_x)        \
+    for (auto el : _x)     \
+        cout << el << ' '; \
+    cout << '\n'
+#define cin_arr(_x)     \
+    for (auto& el : _x) \
+        cin >> el;
+#define fast_input ios_base::sync_with_stdio(0); cin.tie(0)
+#define setpr(_x) cout << setprecision(_x) << fixed
+#define debug(x) cout << __FUNCTION__ << ": " << #x " = " << (x) << endl
+
+struct r {
+    int x, y;
+};
+
+const int MAXN = 1e6;
+int n;
+r p[MAXN];
+int s;
+
+void solve () {
+    cin >> s >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> p[i].x >> p[i].y;
+    }
+}
+
+int main() {
+    fast_input;
+#ifdef __APPLE__
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+    solve();
+}
