bd52fac2c41e85d748901dcbb9b45959c1f7c6bf
Nasedkin Dmitry <jamesnasedkin@gmail.com>
2025-01-12 20:38:05 +0300

    changes

--- tasks/dp/tree_construction/main.cpp
+++ tasks/dp/tree_construction/main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <vector>
+#include <set>
 using namespace std;
+
+const int INF = 1e9;
 
 const int MAXN = 3e5;
 int n;
 vector<int> g[MAXN];
 int diam[MAXN];
 int rad[MAXN];
+
+int ans = INF;
 
 void dfs(int v, int p = -1) {
     int max_r1 = -1, max_r2 = -1;
@@ -28,6 +33,37 @@ void dfs(int v, int p = -1) {
     diam[v] = max(diam[v], max_r1 + max_r2 + 2);
 }
 
+void calc(int v, int p = -1) {
+    set<pair<int, int>> rads;
+    set<pair<int, int>> diams;
+    for (int u : g[v]) {
+        rads.insert({rad[u], u});
+        diams.insert({diam[u], u});
+    }
+
+    for (int u : g[v]) {
+        if (u == p) {
+            continue;
+        }
+        // delete edge v, u
+        rads.erase({rad[u], u});
+        diams.erase({diam[u], u});
+
+        // find_diam1(v), find_diam2(u)
+        int rad1_v = (rads.size() == 0 ? -1 : (--rads.end())->first);
+        int rad2_v = (rads.size() < 2 ? -1 : (--(--rads.end()))->first);
+        int diam_v = (diams.size() == 0 ? 0 : (--diams.end())->first);
+        int diam1 = max(rad1_v + rad2_v + 2, diam_v);
+
+        int diam2 = diam[u];
+        ans = min(ans, max(diam1, max(diam2, 1 + (diam1 + 1) / 2 + (diam2 + 1) / 2)));
+
+        // insert edge v, u
+        rads.insert({rad[u], u});
+        diams.insert({diam[u], u});
+    }
+}
+
 int main() {
     cin >> n;
     for (int i = 1; i < n; i++) {
@@ -39,5 +75,6 @@ int main() {
     }
 
     dfs(0);
-    
+
+    calc(0);
 }
