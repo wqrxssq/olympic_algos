a8431706a60d4455369763f9b13ade01d3ebcf5a
Nasedkin Dmitry <jamesnasedkin@gmail.com>
2025-01-12 21:41:46 +0300

    some changes

--- tasks/dp/tree_construction/main.cpp
+++ tasks/dp/tree_construction/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 const int INF = 1e9;
 
-const int MAXN = 3e5;
+const int MAXN = 4e5;
 int n;
 vector<int> g[MAXN];
 int diam[MAXN];
@@ -50,10 +50,10 @@ void calc(int v, int p = -1) {
         diams.erase({diam[u], u});
 
         // find_diam1(v), find_diam2(u)
-        int rad1_v = (rads.size() == 0 ? -1 : (--rads.end())->first);
-        int rad2_v = (rads.size() < 2 ? -1 : (--(--rads.end()))->first);
+        int rad1_v = (rads.size() == 0 ? -1 : (--rads.end())->first) + 1;
+        int rad2_v = (rads.size() < 2 ? -1 : (--(--rads.end()))->first) + 1;
         int diam_v = (diams.size() == 0 ? 0 : (--diams.end())->first);
-        int diam1 = max(rad1_v + rad2_v + 2, diam_v);
+        int diam1 = max(rad1_v + rad2_v, diam_v);
 
         int diam2 = diam[u];
         ans = min(ans, max(diam1, max(diam2, 1 + (diam1 + 1) / 2 + (diam2 + 1) / 2)));
@@ -61,10 +61,31 @@ void calc(int v, int p = -1) {
         // insert edge v, u
         rads.insert({rad[u], u});
         diams.insert({diam[u], u});
+
+        int old_radv = rad[v];
+        int old_diamv = diam[v];
+        int old_radu = rad[u];
+        int old_diamu = diam[u];
+
+        rad[v] = rad1_v;
+        diam[v] = diam1;
+        rad[u] = max(rad[u], rad[v] + 1);
+        diam[u] = max(diam[u], max(diam[v], rad[v] + rad[u] + 1));
+
+        calc(u, v);
+
+        rad[v] = old_radv;
+        diam[v] = old_diamv;
+        rad[u] = old_radu;
+        diam[u] = old_diamu;
     }
 }
 
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
     cin >> n;
     for (int i = 1; i < n; i++) {
         int v, u;
@@ -77,4 +98,6 @@ int main() {
     dfs(0);
 
     calc(0);
+
+    cout << ans << '\n';
 }
