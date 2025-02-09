cf8c44bc6902e49f3554ba9160c2a1d01f9eb1a3
Nasedkin Dmitry <jamesnasedkin@gmail.com>
2025-02-09 23:43:13 +0300

    c

--- tasks/acade_work/main.cpp
+++ tasks/acade_work/main.cpp
@@ -1,93 +1,128 @@
 #include <iostream>
-#include <queue>
-#include <algorithm>
 #include <vector>
-#include <map>
+
 using namespace std;
 
-int M, N, D, ns, nc, nj;
-char temp['z' + 1], o;
-int mas[20][20];
-int go[20][20][20][20];
-bool used[20][20][101];
-pair<int, int> S, G;
-int dy[] = {0, 1, 0, -1}, dx[] = {-1, 0, 1, 0};
+const int SIZE = 16;
+char grid[SIZE][SIZE];
 
-bool isover(int y, int x) {
-    return y < 0 || y >= M || x < 0 || x >= N;
-}
-
-bool isred(int y, int x, int time, bool istate) {
-    return ~mas[y][x] && !istate ^ time / mas[y][x] & 1;
-}
-
-int Dijkstra() {
-    fill_n(**used, 20 * 20 * 101, false);
-    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> que;
-    que.push(make_pair(0, S)); // time, coordinates
-
-    while (!que.empty()) {
-        pair<int, pair<int, int>> p = que.top();
-        que.pop();
-        if (p.second == G) return p.first; // reached goal
-        if (used[p.second.first][p.second.second][p.first]) {
-            continue;
+bool check(int i, int j) {
+    for (int k = 0; k < SIZE; k++) {
+        if (grid[i][k] != '-' && grid[i][k] == grid[i][j] && k != j) {
+            return false;
         }
-        used[p.second.first][p.second.second][p.first] = 1;
-        for (int i = 0; i < 4; i++) {
-            int ny = p.second.first + dy[i], nx = p.second.second + dx[i];
-            if (isover(ny, nx)) continue; // went outside
-            if (go[p.second.first][p.second.second][ny][nx] == -1) continue; // under construction
-            int nexttime = p.first + D + go[p.second.first][p.second.second][ny][nx];
-            if (isred(ny, nx, nexttime, i & 1)) continue; // cannot enter intersection
-            que.push(make_pair(nexttime, make_pair(ny, nx)));
+        if (grid[k][j] != '-' && grid[k][j] == grid[i][j] && k != i) {
+            return false;
         }
     }
-    return -1;
+    int block_i = i / 4;
+    int block_j = j / 4;
+    for (int k = block_i * 4; k < block_i * 4 + 4; k++) {
+        for (int l = block_j * 4; l < block_j * 4 + 4; l++) {
+            if (i == k && j == l) {
+                continue;
+            }
+            if (grid[k][l] != '-' && grid[k][l] == grid[i][j]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+vector<int> get_forbidden(int i, int j) {
+    vector<int> forbidden(SIZE, false);
+    for (int k = 0; k < SIZE; k++) {
+        if (grid[i][k] != '-') {
+            forbidden[grid[i][k] - 'A'] = true;
+        }
+        if (grid[k][j] != '-') {
+            forbidden[grid[k][j] - 'A'] = true;
+        }
+    }
+    int block_i = i / 4;
+    int block_j = j / 4;
+    for (int k = block_i * 4; k < block_i * 4 + 4; k++) {
+        for (int l = block_j * 4; l < block_j * 4 + 4; l++) {
+            if (i == k && j == l) {
+                continue;
+            }
+            if (grid[k][l] != '-') {
+                forbidden[grid[k][l] - 'A'] = true;
+            }
+        }
+    }
+    return forbidden;
+}
+
+bool dfs(int i, int j) {
+    if (i == SIZE) {
+        return true;
+    }
+    if (check(i, j)) {
+        int next_i = i;
+        int next_j = j + 1;
+        for (; next_i < SIZE; next_i++) {
+            for (; next_j < SIZE; next_j++) {
+                if (grid[next_i][next_j] == '-') {
+                    break;
+                }
+            }
+            if (next_j < SIZE) {
+                break;
+            }
+            next_j = 0;
+        }
+        vector<int> forbidden = get_forbidden(i, j);
+        for (int c = 0; c < SIZE; c++) {
+            if (forbidden[c]) {
+                continue;
+            }
+            grid[i][j] = 'A' + c;
+            if (dfs(next_i, next_j)) {
+                return true;
+            }
+            grid[i][j] = '-';
+        }
+    }
+    return false;
+}
+
+void solve() {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (grid[i][j] == '-') {
+                if (dfs(i, j)) {
+                    cout << "NO SOLUTION\n";
+                    return;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            cout << grid[i][j];
+        }
+        cout << '\n';
+    }
 }
 
 int main() {
-    for (int i = 'a', j = 1; i <= 'z'; i++, j++) { // map-like
-        temp[i] = j;
-    }
-
-    while (cin >> M >> N, M) {
-        fill_n(*mas, 400, -1);
-        fill_n(***go, 20 * 20 * 20 * 20, 0);
-
-        cin >> D;
-        cin >> ns;
-        for (int i = 0; i < ns; i++) {
-            char h;
-            int v, k;
-            cin >> h >> o >> v >> k;
-            mas[temp[h] - 1][v - 1] = k;
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+    string s;
+    int i = 0;
+    while (cin >> s) {
+        for (int j = 0; j < 16; j++) {
+            grid[i][j] = s[j];
         }
-
-        cin >> nc;
-        for (int i = 0; i < nc; i++) {
-            char h1, h2;
-            int v1, v2;
-            cin >> h1 >> o >> v1 >> h2 >> o >> v2; // input is troublesome
-            go[temp[h1] - 1][v1 - 1][temp[h2] - 1][v2 - 1] = -1;
-            go[temp[h2] - 1][v2 - 1][temp[h1] - 1][v1 - 1] = -1;
+        i++;
+        if (i == 16) {
+            solve();
+            i = 0;
         }
-
-        cin >> nj;
-        for (int i = 0; i < nj; i++) {
-            char h1, h2;
-            int v1, v2, d;
-            cin >> h1 >> o >> v1 >> h2 >> o >> v2 >> d;
-            go[temp[h1] - 1][v1 - 1][temp[h2] - 1][v2 - 1] = d;
-            go[temp[h2] - 1][v2 - 1][temp[h1] - 1][v1 - 1] = d;
-        }
-
-        char h1, h2;
-        int v1, v2;
-        cin >> h1 >> o >> v1 >> h2 >> o >> v2;
-        S = make_pair(temp[h1] - 1, v1 - 1);
-        G = make_pair(temp[h2] - 1, v2 - 1);
-
-        cout << Dijkstra() << endl;
     }
 }
