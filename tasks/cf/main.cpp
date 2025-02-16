af34289bdc5318cf21205617604ae7bc8d752887
Nasedkin Dmitry <jamesnasedkin@gmail.com>
2025-02-16 12:59:08 +0300

    change_el + check_is_palindrom_on_segment task

--- tasks/cf/main.cpp
+++ tasks/cf/main.cpp
@@ -1,137 +1,125 @@
 #pragma GCC optimize("O3,unroll-loops")
-
-#include <math.h>
-
-#include <algorithm>
-#include <bitset>
-#include <cassert>
-#include <chrono>
-#include <cstring>
-#include <deque>
-#include <iomanip>
 #include <iostream>
-#include <map>
-#include <queue>
-#include <random>
-#include <set>
-#include <stack>
 #include <string>
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
-#include <climits>
 using namespace std;
 
-using uint = unsigned int;
 using ll = long long;
 using ull = unsigned long long;
-using vi = vector<int>;
-using vvi = vector<vi>;
-using vll = vector<ll>;
-using pii = pair<int, int>;
-using pll = pair<ll, ll>;
-using vpii = vector<pii>;
-const double PI = acos(-1);
-const double EPS = 1e-6;
-const int INF = 1e9;
-const ll INFLL = 1e18;
-const int MOD = 1e9 + 7;
 
-mt19937 rnd(static_cast<unsigned int>(chrono::steady_clock().now().time_since_epoch().count()));
-
-#define all(_x) _x.begin(), _x.end()
-#define rall(_x) _x.rbegin(), _x.rend()
-#define pb push_back
-#define ff first
-#define ss second
-#define sz(_x) (int)_x.size()
-#define display(_x)        \
-    for (auto el : _x)     \
-        cout << el << ' '; \
-    cout << '\n'
-#define cin_arr(_x)     \
-    for (auto& el : _x) \
-        cin >> el;
-#define fast_input ios_base::sync_with_stdio(0); cin.tie(0)
-#define setpr(_x) cout << setprecision(_x) << fixed
-#define debug(x) cout << __FUNCTION__ << ": " << #x " = " << (x) << endl
-
+const int MAXN = 100000;
+const int MOD = 1000000007;
 const int power = 31;
-const int MAXN = 1e5;
+
 int n;
-ll p[MAXN];
-pll t[MAXN << 2];
 string s;
 
+ull p64[MAXN];
+ll pMod[MAXN];
+
+struct Node {
+    ull f64, r64;
+    ll fMod, rMod;
+};
+
+Node t[MAXN << 2];
+
 void init() {
-    p[0] = 1;
+    p64[0] = 1;
+    pMod[0] = 1;
     for (int i = 1; i < n; i++) {
-        p[i] = p[i - 1] * power;
+        p64[i] = p64[i - 1] * power;
+        pMod[i] = (pMod[i - 1] * power) % MOD;
     }
 }
 
 void build(int tl, int tr, int v) {
     if (tl == tr) {
-        t[v] = {s[tl] - 'a' + 1, s[tl] - 'a' + 1};
+        int val = s[tl] - 'a' + 1;
+        t[v].f64 = val;
+        t[v].r64 = val;
+        t[v].fMod = val;
+        t[v].rMod = val;
     } else {
         int tm = (tl + tr) >> 1;
         build(tl, tm, v << 1);
         build(tm + 1, tr, v << 1 | 1);
-        t[v] = {t[v << 1].ff * p[tr - tm] + t[v << 1 | 1].ff,
-               t[v << 1].ss + t[v << 1 | 1].ss * p[tm - tl + 1]};
+        int rightLen = tr - tm;
+        int leftLen = tm - tl + 1;
+        t[v].f64 = t[v << 1].f64 * p64[rightLen] + t[v << 1 | 1].f64;
+        t[v].fMod = (t[v << 1].fMod * pMod[rightLen] + t[v << 1 | 1].fMod) % MOD;
+        t[v].r64 = t[v << 1].r64 + t[v << 1 | 1].r64 * p64[leftLen];
+        t[v].rMod = (t[v << 1].rMod + t[v << 1 | 1].rMod * pMod[leftLen]) % MOD;
     }
 }
 
 void change(int tl, int tr, int v, int pos, int val) {
     if (tl == tr) {
-        t[v] = {val, val};
+        t[v].f64 = val;
+        t[v].r64 = val;
+        t[v].fMod = val;
+        t[v].rMod = val;
     } else {
         int tm = (tl + tr) >> 1;
-        if (pos <= tm) {
+        if (pos <= tm)
             change(tl, tm, v << 1, pos, val);
-        } else {
+        else
             change(tm + 1, tr, v << 1 | 1, pos, val);
-        }
-        t[v] = {t[v << 1].ff * p[tr - tm] + t[v << 1 | 1].ff,
-               t[v << 1].ss + t[v << 1 | 1].ss * p[tm - tl + 1]};
+        int rightLen = tr - tm;
+        int leftLen = tm - tl + 1;
+        t[v].f64 = t[v << 1].f64 * p64[rightLen] + t[v << 1 | 1].f64;
+        t[v].fMod = (t[v << 1].fMod * pMod[rightLen] + t[v << 1 | 1].fMod) % MOD;
+        t[v].r64 = t[v << 1].r64 + t[v << 1 | 1].r64 * p64[leftLen];
+        t[v].rMod = (t[v << 1].rMod + t[v << 1 | 1].rMod * pMod[leftLen]) % MOD;
     }
 }
 
-pll get_hashes(int tl, int tr, int v, int l, int r) {
+Node combineNodes(const Node &left, const Node &right, int leftLen, int rightLen) {
+    Node res;
+    res.f64 = left.f64 * p64[rightLen] + right.f64;
+    res.fMod = (left.fMod * pMod[rightLen] + right.fMod) % MOD;
+    res.r64 = left.r64 + right.r64 * p64[leftLen];
+    res.rMod = (left.rMod + right.rMod * pMod[leftLen]) % MOD;
+    return res;
+}
+
+Node get_hashes(int tl, int tr, int v, int l, int r) {
     if (tl == l && tr == r) {
         return t[v];
     }
     int tm = (tl + tr) >> 1;
     if (r <= tm) {
         return get_hashes(tl, tm, v << 1, l, r);
-    }
-    if (l > tm) {
+    } else if (l > tm) {
         return get_hashes(tm + 1, tr, v << 1 | 1, l, r);
+    } else {
+        Node leftNode = get_hashes(tl, tm, v << 1, l, tm);
+        Node rightNode = get_hashes(tm + 1, tr, v << 1 | 1, tm + 1, r);
+        int leftLen = tm - l + 1;
+        int rightLen = r - tm;
+        return combineNodes(leftNode, rightNode, leftLen, rightLen);
     }
-    pll hl = get_hashes(tl, tm, v << 1, l, tm);
-    pll hr = get_hashes(tm + 1, tr, v << 1 | 1, tm + 1, r);
-    return {hl.ff * p[r - tm] + hr.ff, 
-            hl.ss + hr.ss * p[tm - l + 1]};
 }
 
 void solve() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
     cin >> s;
-    n = sz(s);
-
+    n = s.size();
     init();
     build(0, n - 1, 1);
-
+    
     int q;
     cin >> q;
-    while (q--) {
-        string m;
-        cin >> m;
-        if (m == "palindrome?") {
+    while(q--) {
+        string op;
+        cin >> op;
+        if(op == "palindrome?") {
             int l, r;
             cin >> l >> r;
             l--; r--;
-            pll h = get_hashes(0, n - 1, 1, l, r);
-            if (h.ff == h.ss) {
+            Node res = get_hashes(0, n - 1, 1, l, r);
+            if(res.f64 == res.r64 && res.fMod == res.rMod) {
                 cout << "Yes\n";
             } else {
                 cout << "No\n";
@@ -141,17 +129,17 @@ void solve() {
             char c;
             cin >> pos >> c;
             pos--;
-            change(0, n - 1, 1, pos, c - 'a' + 1);
+            int val = c - 'a' + 1;
+            change(0, n - 1, 1, pos, val);
         }
     }
 }
 
-
 int main() {
-    fast_input;
 #ifdef __APPLE__
     freopen("in.txt", "r", stdin);
     freopen("out.txt", "w", stdout);
 #endif
     solve();
+    return 0;
 }
