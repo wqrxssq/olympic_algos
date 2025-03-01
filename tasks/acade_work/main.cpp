c16a8d9e82a07abee503a9bcddf138f0235d9d3c
Nasedkin Dmitry <jamesnasedkin@gmail.com>
2025-03-01 22:18:28 +0300

    bad feelings

--- tasks/acade_work/main.cpp
+++ tasks/acade_work/main.cpp
@@ -1,49 +1,307 @@
+// #pragma GCC optimize("O3")
+// #pragma GCC optimize("unroll-loops")
 #include <iostream>
-#include <set>
 #include <vector>
-#include <algorithm>
-#include <queue>
+#include <random>
+#include <chrono>
+#include <iomanip>
+#include <set>
+ 
 using namespace std;
 
-typedef long long ll;
-const ll INFLL = 1e18;
-struct seg {
-    int l, r, c;
-};
+// #define int long long
+// #define int128 __int128
+// #define ld long double
+// #define float128 __float128
+// #define all(x) x.begin(), x.end()
+// #define pb push_back
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
-// 2 1 1 1 2 2 2
+const int MAXN = 1000, K = 30, PW = 179, PW1 = 37, MOD = 1e9 + 1000 - 7, MOD1 = 1e9 + 7, INF = 2e9;
+std::mt19937 engine(chrono::steady_clock::now().time_since_epoch().count());
 
-// 3 1 2 1 3 2 1 2 4 3
+int n;
+vector<vector<int>> g;
 
-void solve() {
-    int n;
-    cin >> n;
-    vector<seg> a(n);
-    for (auto &[l, r, c] : a) {
-        int t;
-        cin >> l >> t >> c;
-        r = l + t;
+int randint(int l, int r) {
+    return engine() % (r - l + 1) + l;
+}
+
+using Edges = std::vector<std::pair<int, int>>;
+
+void printEdges(Edges edges) {
+    g.assign(n + 1, {});
+    std::shuffle(edges.begin(), edges.end(), engine);
+    std::vector<int> labels(edges.size() + 1);
+    std::iota(labels.begin(), labels.end(), 1);
+    std::shuffle(labels.begin(), labels.end(), engine);
+    for (const auto& edge : edges) {
+        int u = labels[edge.first];
+        int v = labels[edge.second];
+        if (engine() % 2) {
+            std::swap(u, v);
+        }
+        // std::cout << u << " " << v << "\n";
+        g[u].push_back(v);
+        g[v].push_back(u);
     }
+}
 
-    sort(a.begin(), a.end(), [](seg &a, seg &b) {
-        return a.r < b.r;
-    });
-    vector<pair<int, ll>> ans;
-    ans.push_back({0, 0});
-    for (auto &[l, r, c] : a) {
-        pair<int, ll> s = {l, INFLL};
-        auto [r_best, c_best] = ans[upper_bound(ans.begin(), ans.end(), s) - ans.begin() - 1];
-        c_best += c;
-        if (ans.back().second < c_best) {
-            ans.push_back({r, c_best});
+void prim(int n) {
+    Edges edges;
+    for (int i = 1; i < n; ++i) {
+        edges.emplace_back(i, randint(0, i - 1));
+    }
+    printEdges(edges);
+}
+
+void primHalf(int n) {
+    Edges edges;
+    for (int i = 1; i < n; ++i) {
+        edges.emplace_back(i, randint(i / 2, i - 1));
+    }
+    printEdges(edges);
+}
+
+void primNOverFour(int n) {
+    Edges edges;
+    for (int i = 1; i < n; ++i) {
+        edges.emplace_back(i, randint(std::max(0, i - n / 4), i - 1));
+    }
+    printEdges(edges);
+}
+
+void primWnext(int n, int w) {
+    auto wnext = [n, w] (int bound) {
+        int x = randint(0, bound - 1);
+        for (int i = 0; i < +w; ++i) {
+            x = std::max(x, randint(0, bound - 1));
+        }
+        for (int i = 0; i < -w; ++i) {
+            x = std::min(x, randint(0, bound - 1));
+        }
+        return x;
+    };
+
+    Edges edges;
+    for (int i = 1; i < n; ++i) {
+        edges.emplace_back(i, wnext(i));
+    }
+    printEdges(edges);
+}
+
+void doKruskal(int n, Edges allEdges) {
+    std::vector<int> dsu(n);
+    std::iota(dsu.begin(), dsu.end(), 0);
+
+    std::function<int(int)> findRoot = [&dsu, &findRoot] (int v) {
+        if (v == dsu[v]) {
+            return v;
+        }
+        return dsu[v] = findRoot(dsu[v]);
+    };
+
+    auto unite = [&dsu, &findRoot] (int u, int v) {
+        u = findRoot(u);
+        v = findRoot(v);
+        if (u == v) {
+            return false;
+        }
+        dsu[u] = v;
+        return true;
+    };
+
+    std::shuffle(allEdges.begin(), allEdges.end(), engine);
+
+    Edges result;
+    for (const auto& edge : allEdges) {
+        if (unite(edge.first, edge.second)) {
+            result.push_back(edge);
         }
     }
 
-    cout << ans.back().second << endl;
+    printEdges(result);
+
 }
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    solve();
+void kruskal(int n) {
+    Edges allEdges;
+    for (int v = 0; v < n; ++v) {
+        for (int u = 0; u < v; ++u) {
+            allEdges.emplace_back(u, v);
+        }
+    }
+
+    doKruskal(n, allEdges);
+}
+
+void kruskalBipartiteEqualParts(int n) {
+    Edges allEdges;
+    for (int v = 0; v < n; ++v) {
+        for (int u = 0; u < v; ++u) {
+            if (u % 2 != v % 2) {
+                allEdges.emplace_back(u, v);
+            }
+        }
+    }
+
+    doKruskal(n, allEdges);
+}
+
+void kruskalBipartiteDifferentParts(int n) {
+    int rightPartSize = randint(std::sqrt(n), std::sqrt(n) * 2);
+
+    Edges allEdges;
+    for (int v = 0; v < n; ++v) {
+        for (int u = 0; u < v; ++u) {
+            if ((u < rightPartSize) != (v < rightPartSize)) {
+                allEdges.emplace_back(u, v);
+            }
+        }
+    }
+
+    doKruskal(n, allEdges);
+}
+
+void prufer(int n, int numZeroes) {
+    std::vector<int> code;
+    for (int i = 0; i < n - 2; ++i) {
+        code.push_back(randint(0, n - 1));
+    }
+
+    std::set<int> zeroes;
+    for (int iter = 0; iter < numZeroes; ++iter) {
+        while (true) {
+            int x = randint(0, n - 3);
+            if (zeroes.insert(x).second) {
+                code[x] = 0;
+                break;
+            }
+        }
+    }
+
+    std::vector<int> degree(code.size() + 2, 1);
+    for (int v: code) {
+        ++degree[v];
+    }
+
+    std::set<int> leaves;
+    for (size_t v = 0; v != degree.size(); ++v) {
+        if (degree[v] == 1) {
+            leaves.insert(v);
+        }
+    }
+
+    Edges result;
+
+    for (int v: code) {
+        assert(!leaves.empty());
+        int u = *leaves.begin();
+        leaves.erase(leaves.begin());
+        if (--degree[v] == 1) {
+            leaves.insert(v);
+        }
+
+        result.emplace_back(v, u);
+    }
+
+    assert(leaves.size() == 2u);
+    result.emplace_back(*leaves.begin(), *leaves.rbegin());
+
+    printEdges(result);
+}
+
+void generate(int n, int index) {
+    switch (index) {
+        case 1:  prim(n); break;
+        case 2:  primHalf(n); break;
+        case 3:  primNOverFour(n); break;
+        case 4:  primWnext(n, 2); break;
+        case 5:  primWnext(n, -2); break;
+        case 6:  kruskal(n); break;
+        case 7:  kruskalBipartiteEqualParts(n); break;
+        case 8:  kruskalBipartiteDifferentParts(n); break;
+        case 9:  prufer(n, 0); break;
+        case 10: prufer(n, std::min(n/10, 20)); break;
+        default: assert(false);
+    }
+}
+int h[MAXN + 1] = {0}, parent[MAXN + 1] = {0};
+
+void dfs(int v, int p, vector<vector<int>> &g) {
+    h[v] = (p != -1 ? h[p] + 1 : 0);
+    parent[v] = p;
+    for (int u : g[v]) {
+        if (u != p) {
+            dfs(u, v, g);
+        }
+    }
+}
+
+double get_average_height(int root, int n, vector<vector<int>>& g) {
+    double res = 0;
+    int cnt = 0;
+    for (int v = 1; v <= n; v++) {
+        cnt += (g[v].size() == 1 && v != root);
+        res += (g[v].size() == 1 && v != root ? h[v] : 0);
+    }
+    return res / cnt;
+}
+
+vector<int> get_diam(vector<vector<int>> &g) {
+    h[1] = 0;
+    dfs(1, 1, g);
+    int best = 1;
+    for (int i = 1; i <= n; ++i) {
+        if (h[best] < h[i])
+            best = i;
+    }
+    h[best] = 0;
+    int start = best;
+    dfs(best, best, g);
+    vector<int> path;
+    for (int i = 1; i <= n; ++i) {
+        if (h[best] < h[i])
+            best = i;
+    }
+    while (best != start) {
+        path.push_back(best);
+        best = parent[best];
+    }
+    path.push_back(start);
+    return path;
+}
+
+long double solve() {
+    vector<vector<int>> g1 = g;
+    vector<int> path = get_diam(g1);
+    int root = path[path.size() / 2];
+    dfs(root, -1, g1);
+    double average_height = get_average_height(root, n, g1);
+    return average_height;
+}
+
+signed main() {
+#ifdef __APPLE__
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout << setprecision(6) << fixed;
+    int T = 1;
+    vector<int> sizes = {10, 25, 50, 100, 200, 300, 400, 500, 750, 1000};
+    for (auto it: sizes) {
+        n = it;
+        cout << "SIZE: " << n << endl << endl;
+        for (int i = 0; i < 10; ++i) {
+            long double mid = 0;
+            for (int j = 0; j < 64; ++j) {
+                generate(n, i + 1);
+                mid += solve();
+            }
+            cout << (i + 1) << ": "  << mid / 64 << endl;
+        }
+        cout << endl << endl;
+    }
 }
--- tasks/cf/main.cpp
+++ tasks/cf/main.cpp
@@ -1,291 +1,251 @@
+// #pragma GCC optimize("O3")
+// #pragma GCC optimize("unroll-loops")
 #include <iostream>
-#include <algorithm>
 #include <vector>
-#include <math.h>
-#include <set>
-#include <unordered_set>
-#include <map>
-#include <unordered_map>
-#include <deque>
-#include <queue>
-#include <string>
-#include <stack>
-#include <cassert>
-#include <iomanip>
 #include <random>
 #include <chrono>
-#include <cstring>
+#include <iomanip>
+#include <cassert>
+ 
 using namespace std;
 
-using ll = long long;
-using ull = unsigned long long;
-using vi = vector <int>;
-using vvi = vector <vi>;
-using vll = vector <ll>;
-using pii = pair <int, int>;
-using vpii = vector <pii>;
-
-#define all(_x) _x.begin(), _x.end()
-#define rall(_x) _x.rbegin(), _x.rend()
-// #define sz(_x) (int)_x.size()
-#define ff first
-#define ss second
-#define pb push_back
-#define display(_x) for (int x : a) cout << x << ' '; cout << '\n';
-#define fast_input ios_base::sync_with_stdio(0); cin.tie(0);
-#define cin_arr(_x) for (int &x : a) cin >> x;
-
-mt19937 rnd((int)chrono::steady_clock::now().time_since_epoch().count());
-
-const int INF = 1e9;
-const int MOD = 1e9 + 7;
-const double EPS = 1e-6;
-
-struct Node {
-    int priority;
-
-    int value, sum, lazy;
-
-    char c;
-    int sz;
-
-    Node* l = nullptr;
-    Node* r = nullptr;
-
-    Node (int _value) : priority(rnd()), value(_value), sum(_value), lazy(1), sz(1) {
-    }
-};
-
-const int POWER = 31;
-const int MAXN = 1e6;
+// #define int long long
+// #define int128 __int128
+// #define ld long double
+// #define float128 __float128
+// #define all(x) x.begin(), x.end()
+// #define pb push_back
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+ 
+const int MAXN = 1000, K = 30, PW = 179, PW1 = 37, MOD = 1e9 + 1000 - 7, MOD1 = 1e9 + 7, INF = 2e9;
 int n;
-int del[MAXN];
 
-string s;
-int p[MAXN], rev_p[MAXN];
-int h[MAXN];
+int h[MAXN + 1];
+int parent[MAXN + 1];
+double EPS = 1e-9;
 
-Node* t;
-int len = 0;
 
-int add(int a,int b) {
-    return a + b >= MOD ? a + b - MOD : a + b;
-}
-
-int mult(int a,int b) {
-    return 1LL * a * b % MOD;
-}
-
-int getSz(Node* t) {
-    if (t == nullptr) {
-        return 0;
-    }
-    return t->sz;
-}
-
-int getSum(Node* t) {
-    if (t == nullptr) {
-        return 0;
-    }
-    return t->sum;
-}
-
-int bpow(int a, int n, int mod) {
-    int res = 1;
-    while (n) {
-        if (n & 1) { 
-            res = 1LL * res * a % mod;
-        }
-        n >>= 1;
-        a = 1LL * a * a % mod;
-    }
-    return res;
-}
-
-void push(Node *t) {
-    if (t == nullptr) {
-        return;
-    }
-    if(t->lazy == 1) {
-        return;
-    }
-
-    if (t->l != nullptr) {
-        t->l->lazy = mult(t->l->lazy, t->lazy);
-        t->l->sum = mult(t->l->sum, t->lazy);
-        t->l->value = mult(t->l->value, t->lazy);
-    }
-
-    if (t->r != nullptr) {
-        t->r->lazy = mult(t->r->lazy, t->lazy);
-        t->r->sum = mult(t->r->sum, t->lazy);
-        t->r->value = mult(t->r->value, t->lazy);
-    }
-
-    t->lazy = 1;
-}
-
-void update(Node *t) {
-    t->sz = getSz(t->l) + getSz(t->r) + 1;
-    t->sum = add(add(getSum(t->l), getSum(t->r)), t->value);
-}
-
-pair<Node*, Node*> split(Node *t, int x) {
-    if (t == nullptr) {
-        return {nullptr, nullptr};
-    }
-    push(t);
-
-    int sz = getSz(t->l) + 1;
-    if (x >= sz) {
-        auto [l, r] = split(t->r, x - sz);
-        t->r = l;
-        update(t);
-        return {t, r};
+vector<int> findBestMethods(int size, double target) {
+    vector<double> methods;
+    if (size == 10) {
+        methods = {2.185565, 2.465885, 3.071615, 2.667708, 1.688393,
+                   2.590848, 2.529762, 2.543601, 2.749182, 2.732403};
+    } else if (size == 25) {
+        methods = {3.181135, 3.878221, 4.340439, 4.025777, 2.173523,
+                   4.065529, 4.107294, 3.512855, 4.529359, 4.408145};
+    } else if (size == 50) {
+        methods = {3.804066, 5.383856, 5.638703, 5.308961, 2.637430,
+                   5.461265, 5.388613, 4.188877, 6.267547, 5.550005};
+    } else if (size == 100) {
+        methods = {4.393432, 6.863311, 6.220531, 6.866039, 3.032581,
+                   7.291268, 7.409089, 5.225061, 8.497712, 6.776584};
+    } else if (size == 200) {
+        methods = {5.348751, 8.771503, 7.214957, 8.621204, 3.350827,
+                   9.492062, 9.446008, 6.210173, 12.069021, 8.504677};
+    } else if (size == 300) {
+        methods = {5.537817, 9.980562, 7.755818, 9.742348, 3.518681,
+                   11.021041, 11.156436, 6.762846, 14.558511, 11.247794};
+    } else if (size == 400) {
+        methods = {5.855149, 10.730117, 8.017621, 10.634304, 3.634407,
+                   12.749966, 12.162976, 7.173903, 16.626668, 13.702798};
+    } else if (size == 500) {
+        methods = {6.080226, 11.563673, 8.181517, 11.204999, 3.897417,
+                   13.737247, 13.179863, 7.716309, 18.600009, 15.719975};
+    } else if (size == 750) {
+        methods = {6.629989, 12.751271, 8.612399, 12.195549, 4.111325,
+                   15.547751, 15.970845, 8.502986, 23.453495, 20.249136};
+    } else if (size == 1000) {
+        methods = {6.846231, 13.578109, 9.060868, 13.156907, 4.222215,
+                   17.620883, 17.377692, 9.279635, 26.439482, 23.697571};
     } else {
-        auto [l, r] = split(t->l, x);
-        t->l = r;
-        update(t);
-        return {l, t};
-    }
-}
-
-Node* merge(Node* l, Node* r) {
-    if (l == nullptr) {
-        return r;
-    }
-    if (r == nullptr) {
-        return l;
+        return {};
     }
 
-    push(l);
-    push(r);
-
-    if (l->priority > r->priority) {
-        l->r = merge(l->r, r);
-        update(l);
-        return l;
-    } else {
-        r->l = merge(l, r->l);
-        update(r);
-        return r;
-    }
-}
-
-Node* erase(int x) {
-    auto [l, r] = split(t, x);
-    auto [l1, r1] = split(r, 1);
-
-    if (r1 != nullptr) {
-        r1->lazy = mult(r1->lazy, rev_p[1]);
-        r1->sum = mult(r1->sum, rev_p[1]);
-        r1->value = mult(r1->value, rev_p[1]);
-    }
-    return merge(l, r1);
-}
-
-Node* insert(int x, int val){
-    auto [l, r] = split(t, x);
-    Node* newNode = new Node(val);
-
-    if(r != nullptr){
-        r->lazy = mult(r->lazy, p[1]);
-        r->sum = mult(r->sum, p[1]);
-        r->value = mult(r->value, POWER);
-    }
-
-    return merge(merge(l, newNode), r);
-}
-
-bool preiod(int x) {
-    auto [l, r] = split(t, len - x);
-    int s1 = l->sum;
-
-    t = merge(l, r);
-
-    auto [l1, r1] = split(t, x);
-    int s2 = r1->sum;
-    t = merge(l1, r1);
-
-    return s1 == mult(s2, rev_p[x]);
-}
-
-void init() {
-    p[0] = 1;
-    rev_p[0] = 1;
-
-    for (int i = 1; i < MAXN; i++) {
-        p[i] = mult(p[i - 1], POWER);
-        rev_p[i] = bpow(p[i], MOD-2, MOD);
-    }
-
-    for (int i = 2; i < MAXN; i++){
-        if (!del[i]) {
-            for (int j = i; j < MAXN; j += i) {
-                if (!del[j]) {
-                    del[j] = i;
-                }
-            }
+    // Определяем минимальную абсолютную разницу между target и значениями
+    double minDiff = 1e9;
+    int best = 0;
+    for (int i = 0; i < methods.size(); i++) {
+        double diff = fabs(methods[i] - target);
+        if (diff < minDiff) {
+            minDiff = min(minDiff, diff);
+            best = i;
         }
+    }
+
+    vector <int> ans;
+    for (int i = 0; i < methods.size(); i++) {
+        double diff = fabs(methods[i] - target);
+        if (fabs(diff - minDiff) <= EPS) {
+            ans.push_back(i + 1);
+        }
+    }
+
+    return ans;
+}
+
+int findBestMethodsDiametr(int size, double target) {
+    vector<double> methods;
+    if (size == 10) {
+        methods = {6.359375, 6.687500, 8.093750, 6.937500, 5.078125,
+                   7.203125, 7.437500, 6.906250, 7.312500, 7.234375};
+    } else if (size == 25) {
+        methods = {9.875000, 11.515625, 12.640625, 11.968750, 6.968750,
+                   12.218750, 12.312500, 10.875000, 13.046875, 12.859375};
+    } else if (size == 50) {
+        methods = {12.437500, 16.171875, 16.765625, 16.359375, 8.562500,
+                   17.468750, 17.640625, 14.093750, 19.359375, 18.015625};
+    } else if (size == 100) {
+        methods = {15.671875, 21.796875, 21.250000, 21.718750, 10.203125,
+                   25.062500, 24.765625, 16.734375, 29.328125, 24.796875};
+    } else if (size == 200) {
+        methods = {18.484375, 25.984375, 24.656250, 27.500000, 11.796875,
+                   33.921875, 34.796875, 21.515625, 42.906250, 32.906250};
+    } else if (size == 300) {
+        methods = {20.515625, 30.156250, 26.843750, 32.359375, 12.609375,
+                   40.984375, 40.265625, 23.218750, 51.078125, 44.781250};
+    } else if (size == 400) {
+        methods = {21.718750, 33.328125, 28.796875, 34.375000, 13.312500,
+                   46.953125, 47.625000, 25.281250, 62.828125, 53.468750};
+    } else if (size == 500) {
+        methods = {22.562500, 35.343750, 29.125000, 37.281250, 13.828125,
+                   51.906250, 51.062500, 28.125000, 70.562500, 62.531250};
+    } else if (size == 750) {
+        methods = {24.609375, 39.125000, 31.671875, 41.125000, 14.671875,
+                   61.656250, 61.156250, 30.375000, 89.875000, 82.406250};
+    } else if (size == 1000) {
+        methods = {26.437500, 41.687500, 32.937500, 43.812500, 15.406250,
+                   68.312500, 69.828125, 33.406250, 101.562500, 96.250000};
+    } else {
+        cout << "Unknown size" << endl;
+        return 1;
+    }
+    
+    double minDiff = 1e9;
+    int best = 0;
+    for (int i = 0; i < methods.size(); i++) {
+        double diff = fabs(methods[i] - target);
+        if (diff < minDiff) {
+            minDiff = min(minDiff, diff);
+            best = i;
+        }
+    }
+
+    return best + 1;
+}
+
+void dfs(int v, int p, vector<vector<int>> &g) {
+    h[v] = (p != -1 ? h[p] + 1 : 0);
+    parent[v] = p;
+    for (int u : g[v]) {
+        if (u != p) {
+            dfs(u, v, g);
+        }
+    }
+}
+
+vector<int> get_diam(vector<vector<int>> &g) {
+    h[1] = 0;
+    dfs(1, 1, g);
+    int best = 1;
+    for (int i = 1; i <= n; ++i) {
+        if (h[best] < h[i])
+            best = i;
+    }
+    h[best] = 0;
+    int start = best;
+    dfs(best, best, g);
+    vector<int> path;
+    for (int i = 1; i <= n; ++i) {
+        if (h[best] < h[i])
+            best = i;
+    }
+    while (best != start) {
+        path.push_back(best);
+        best = parent[best];
+    }
+    path.push_back(start);
+    return path;
+}
+
+
+double get_average_height(int root, int n, vector<vector<int>>& g) {
+    double res = 0;
+    int cnt = 0;
+    for (int v = 1; v <= n; v++) {
+        cnt += (g[v].size() == 1 && v != root);
+        res += (g[v].size() == 1 && v != root ? h[v] : 0);
+    }
+    return res / cnt;
+}
+
+void DFS_GOIDA(int u, int p, vector<vector<int>> &g, vector<int> &c) {
+    for (auto it: g[u]) {
+        if (p == it)
+            continue;
+        c[it] = (c[u] ^ 1);
+        DFS_GOIDA(it, u, g, c);
+    }
+}
+
+int pred_6_7(vector<vector<int>> &g) {
+    vector<int> coloring(n + 1, 0);
+    DFS_GOIDA(1, -1, g, coloring);
+    int c0 = 0, c1 = 0;
+    for (int i = 1; i <= n; ++i) {
+        if (coloring[i])
+            c1++;
+        else
+            c0++;
+    }
+    if (abs(c0 - c1) <= 1) {
+        return {7};
+    } else {
+        return {6};
     }
 }
 
 void solve() {
-    init();
-    cin >> n >> s;
-
-    t = nullptr;
-    for (int i = 0; i < n; i++) {
-        h[i] = mult((s[i] - 'a' + 1), p[i+1]);
-        Node* newNode = new Node(h[i]);
-        t = merge(t, newNode);
+    vector<vector<int>> g(n + 1);
+    for (int i = 0; i < n - 1; ++i) {
+        int u, v;
+        cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
     }
 
-    len = n;
-    del[1] = 1;
+    vector<int> path = get_diam(g);
+    int root = path[path.size() / 2];
+    dfs(root, -1, g);
+    double average_height = get_average_height(root, n, g);
+    
+    int b = findBestMethodsDiametr(n, path.size());
+    if (b == 6 || b == 7) {
+        b = pred_6_7(g);
+    }
 
-    int q;
-    cin >> q;
-    while (q--) {
-        char tp;
-        int x;
-        cin >> tp;
-        if (tp == '?') {
-            if (len == 1) {
-                cout << 1 << '\n';
-            } else if (preiod(1)) {
-                cout << 1 << '\n';
-            } else if (del[len] == len) {
-                cout << len << '\n';
-            } else {
-                int x = len;
-                int x1 = len;
-                while (x1 != 1) {
-                    if (preiod(x / del[x1])) {
-                        x /= del[x1];
-                    }
-                    x1 /= del[x1];
-                }
-                cout << x << '\n';
-            }
-        } else if (tp == '-') {
-            cin >> x;
-            --len;
-            t = erase(x);
+    if (b == 3 || b == 8) {
+        if (rng() % 2 == 0) {
+            b = 3;
         } else {
-            char c;
-            cin >> x >> c;
-            len++;
-            t = insert(x, mult((c - 'a' + 1), p[x + 1]));
+            b = 8;
         }
     }
+
+    cout << "1\n";
+    cout << b << '\n';
 }
 
-int main() {
-    fast_input;
+signed main() {
 #ifdef __APPLE__
     freopen("in.txt", "r", stdin);
     freopen("out.txt", "w", stdout);
 #endif
-    solve();
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout << setprecision(6) << fixed;
+    int T = 1;
+    cin >> T; cin >> n;   
+    while (T--) {
+        solve();
+    }
 }
