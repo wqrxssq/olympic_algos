c440151d251fe05ae82642cb4a4dbd0ae25d9672
Nasedkin Dmitry <jamesnasedkin@gmail.com>
2025-03-09 21:38:58 +0300

    alpha-beta-pruning

--- tasks/acade_work/main.cpp
+++ tasks/acade_work/main.cpp
@@ -1,114 +1,98 @@
+// #pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+  
 #include <iostream>
-#include <sstream>
+#include <algorithm>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <math.h>
 #include <vector>
 #include <string>
-#include <algorithm>
+#include <stack>
+#include <cassert>
+#include <random>
+#include <chrono>
+#include <cstring>
 using namespace std;
+     
+#define all(_x) _x.begin(), _x.end()
+#define rall(_x) _x.rbegin(), _x.rend()
+#define pb push_back
+#define sz(_x) (int)_x.size()
+#define ff first
+#define ss second
+#define cin_arr(a)for (int _ii = 0; _ii < (int)a.size(); _ii++)  \
+    cin >> a[_ii];
+#define display(a) for (auto _ii : a)                            \
+    cout << _ii << ' ';                                          \
+cout << '\n';
+#define display_pairs(a) for (auto _ii : a)                      \
+    cout << _ii.ff << ' ' << _ii.ss << '\n';
+#define display_matrix(a) for (auto _row : a)                    \
+{                                                                \
+    for (auto _cell : _row)                                      \
+        cout << _cell << ' ';                                    \
+    cout << '\n';                                                \
+}                                                                \
+cout << '\n'
+#define fast_input ios_base::sync_with_stdio(0); cin.tie(0)
+#define fast_interactive_input ios_base::sync_with_stdio(0)
+
 typedef long long ll;
-void trim(vector<ll>& poly){
-    while(poly.size()>1 && poly.back()==0) poly.pop_back();
-}
-vector<ll> conv(const vector<ll>& a, const vector<ll>& b){
-    vector<ll> res(a.size()+b.size()-1,0);
-    for(size_t i=0;i<a.size();i++)
-        for(size_t j=0;j<b.size();j++)
-            res[i+j]+=a[i]*b[j];
-    return res;
-}
-vector<ll> poly_add(const vector<ll>& a, const vector<ll>& b){
-    size_t n=max(a.size(),b.size());
-    vector<ll> res(n,0);
-    for(size_t i=0;i<n;i++){
-        ll A=i<a.size()?a[i]:0;
-        ll B=i<b.size()?b[i]:0;
-        res[i]=A+B;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair <ll, ll> pll;
+typedef vector <pii> vpii;
+typedef vector <vi> vvi;
+
+const int INF = 1e9;
+const ll INFLL = 1e18;
+const int mod = 1e9 + 7;
+const double eps = 1e-6;
+
+const int MAXN = 1e5;
+
+vi g[MAXN];
+int is_terminal[MAXN];
+int cost[MAXN];
+
+int alpha_beta_pruning(int v, int alpha = -INF, int beta = INF, bool is_max_play = true) {
+    if (is_terminal[v]) {
+        return cost[v];
     }
-    trim(res);
-    return res;
-}
-vector<ll> poly_sub(const vector<ll>& a, const vector<ll>& b){
-    size_t n=max(a.size(),b.size());
-    vector<ll> res(n,0);
-    for(size_t i=0;i<n;i++){
-        ll A=i<a.size()?a[i]:0;
-        ll B=i<b.size()?b[i]:0;
-        res[i]=A-B;
-    }
-    trim(res);
-    return res;
-}
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    string line;
-    vector<ll> s;
-    if(getline(cin,line)){
-        istringstream iss(line);
-        ll num;
-        while(iss >> num) s.push_back(num);
-    }
-    int k = s.size();
-    ll C;
-    vector<ll> lam;
-    if(getline(cin,line)){
-        istringstream iss(line);
-        iss >> C;
-        for (int i = 0; i < k; i++){
-            ll t;
-            iss >> t;
-            lam.push_back(t);
+    if (is_max_play) {
+        for (int u : g[v]) {
+            alpha = max(alpha, alpha_beta_pruning(u, alpha, beta, is_max_play ^ 1));
+            // beta pruning, min won't go to this vertex
+            if (alpha >= beta) {
+                return alpha;
+            }
         }
-    }
-    if(k==0){
-        vector<ll> P = {C};
-        vector<ll> Q = {1, -1};
-        trim(P);
-        trim(Q);
-        for(auto x : P) cout << x << " ";
-        cout << "\n";
-        for(auto x : Q) cout << x << " ";
-        cout << "\n";
-        return 0;
-    }
-    vector<ll> A(k,0);
-    for (int i=0;i<k;i++){
-        A[i]= s[i];
-    }
-    vector<ll> U(k,0);
-    for (int i=0;i<k;i++){
-        for (int j=0;j<i;j++){
-            int idx=(k-i)+j;
-            if(idx>=U.size()) U.resize(idx+1,0);
-            U[idx]+=lam[i]*s[j];
+        return alpha;
+    } else {
+        for (int u : g[v]) {
+            beta = min(beta, alpha_beta_pruning(u, alpha, beta, is_max_play ^ 1));
+            // alpha pruning, max won't go to this vertex
+            if (alpha >= beta) {
+                return beta;
+            }
         }
+        return beta;
     }
-    while(A.size()<U.size()) A.push_back(0);
-    vector<ll> Aprime=poly_sub(A,U);
-    vector<ll> one_minus_x = {1, -1};
-    vector<ll> P_full = conv(one_minus_x, Aprime);
-    if(P_full.size()<= (size_t)k) P_full.resize(k+1,0);
-    P_full[k]+= C;
-    vector<ll> T(k+1,0);
-    T[0]=0;
-    for (int d=1; d<=k; d++){
-        T[d]= lam[k-d];
-    }
-    vector<ll> L(T.size(),0);
-    L[0]=1;
-    for(size_t i=1;i<T.size();i++){
-        L[i] = -T[i];
-    }
-    vector<ll> Q_full = conv(one_minus_x, L);
-    vector<ll> P_final = P_full, Q_final = Q_full;
-    if(Q_final[0]>0){
-        for(auto &x:P_final) x=-x;
-        for(auto &x:Q_final) x=-x;
-    }
-    trim(P_final);
-    trim(Q_final);
-    for(auto x:P_final) cout<<x<<" ";
-    cout<<"\n";
-    for(auto x:Q_final) cout<<x<<" ";
-    cout<<"\n";
-    return 0;
+}
+
+void solve() {
+    int n;
+    cin >> n >> a >> b >> c >> m;
+    cout << alpha_beta_pruning(0, (1 << n) - 1) << '\n';
+}
+
+int main() {
+    fast_input;
+    solve();
 }
