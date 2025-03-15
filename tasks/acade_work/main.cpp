9b9c04ce620a43f1485d7f8daeb2de32dfdd7a92
Nasedkin Dmitry <jamesnasedkin@gmail.com>
2025-03-15 04:44:17 +0300

    another splay

--- tasks/acade_work/main.cpp
+++ tasks/acade_work/main.cpp
@@ -1,237 +1,207 @@
 #include <iostream>
 #include <algorithm>
-#include <set>
-#include <unordered_set>
-#include <map>
-#include <unordered_map>
-#include <queue>
-#include <math.h>
 #include <vector>
-#include <string>
-#include <stack>
-#include <cassert>
-#include <random>
-#include <chrono>
-#include <cstring>
-#include <iomanip>
+#include <utility>
 using namespace std;
 
-using ll = long long;
-using ull = unsigned long long;
-using vi = vector<int>;
-using vll = vector<ll>;
-using pii = pair<int, int>;
-using pll = pair <ll, ll>;
-using vpii = vector <pii>;
-using vvi = vector <vi>;
-
 const int INF = 1e9;
-const ll INFLL = 1e18;
-const int mod = 1e9 + 7;
-const double eps = 1e-8;
+const int MAXN = 100000;
 
 struct SplayTree {
     int size;
     int val, min_el;
-    bool is_rev = false;
-    SplayTree *L = 0, *R = 0, *P = 0;
-
+    bool is_rev;
+    int L, R, P;
+    
     SplayTree() {}
-    SplayTree(int _val) : size(1), val(_val), min_el(_val) {}
+    SplayTree(int _val) : size(1), val(_val), min_el(_val), is_rev(false), L(-1), R(-1), P(-1) {}
 };
 
-int get_size(SplayTree* v) {
-    return (v ? v->size : 0);
-}
-int get_min(SplayTree* v) {
-    return (v ? v->min_el : INF);
+SplayTree t[MAXN];
+int nodes_count = 0;
+
+int new_node(int val) {
+    t[nodes_count] = SplayTree(val);
+    return nodes_count++;
 }
 
-void update(SplayTree* v) {
-    v->size = get_size(v->L) + get_size(v->R) + 1;
-    v->min_el = min(min(get_min(v->L), get_min(v->R)), v->val);
+int get_size(int v) {
+    return (v == -1 ? 0 : t[v].size);
 }
 
-void push(SplayTree* v) {
-    if (v->is_rev) {
-        if (v->L) v->L->is_rev = true;
-        if (v->R) v->R->is_rev = true;
-        swap(v->L, v->R);
-        v->is_rev = false;
+int get_min_val(int v) {
+    return (v == -1 ? INF : t[v].min_el);
+}
+
+void update(int v) {
+    if(v == -1) return;
+    t[v].size = get_size(t[v].L) + get_size(t[v].R) + 1;
+    t[v].min_el = min({ get_min_val(t[v].L), get_min_val(t[v].R), t[v].val });
+}
+
+void push(int v) {
+    if(v == -1) return;
+    if (t[v].is_rev) {
+        if (t[v].L != -1) t[t[v].L].is_rev ^= 1;
+        if (t[v].R != -1) t[t[v].R].is_rev ^= 1;
+        swap(t[v].L, t[v].R);
+        t[v].is_rev = false;
     }
 }
 
-void set_parent(SplayTree* child, SplayTree* parent) {
-    if (child) child->P = parent;
+void set_parent(int child, int parent) {
+    if(child != -1)
+        t[child].P = parent;
 }
-void update_parent(SplayTree* child, SplayTree* old_child) {
-    SplayTree* parent = old_child->P;
-    if (parent) {
-        if (parent->L == old_child) {
-            parent->L = child;
-        } else {
-            parent->R = child;
-        }
+
+void update_parent(int child, int old_child) {
+    int parent = t[old_child].P;
+    if(parent != -1) {
+        if(t[parent].L == old_child)
+            t[parent].L = child;
+        else
+            t[parent].R = child;
     }
 }
 
-void zig(SplayTree* v) {
-    SplayTree* p = v->P;
-    if (p->L == v) {
-        SplayTree* B = v->R;
-
-        v->P = p->P;
+void zig(int v) {
+    int p = t[v].P;
+    if(p == -1) return;
+    if(t[p].L == v) {
+        int B = t[v].R;
+        t[v].P = t[p].P;
         update_parent(v, p);
-
-        v->R = p;
-
-        p->P = v;
-        p->L = B;
-
+        t[v].R = p;
+        t[p].P = v;
+        t[p].L = B;
         set_parent(B, p);
-    } else { // p->R == v
-        SplayTree* B = v->L;
-
-        v->P = p->P;
+    } else {
+        int B = t[v].L;
+        t[v].P = t[p].P;
         update_parent(v, p);
-
-        v->L = p;
-
-        p->P = v;
-        p->R = B;
-
+        t[v].L = p;
+        t[p].P = v;
+        t[p].R = B;
         set_parent(B, p);
     }
     update(p);
     update(v);
 }
 
-void zig_zig(SplayTree* v) {
-    zig(v->P);
+void zig_zig(int v) {
+    zig(t[v].P);
     zig(v);
 }
 
-void zig_zag(SplayTree* v) {
+void zig_zag(int v) {
     zig(v);
     zig(v);
 }
 
-void splay(SplayTree* v) {
-    while (v->P) {
-        SplayTree* p = v->P;
-        if (!p->P) {
+void splay(int v) {
+    while(t[v].P != -1) {
+        int p = t[v].P;
+        if(t[p].P == -1)
             zig(v);
-        } else {
-            SplayTree* g = p->P;
-            bool zigzig = (g->L == p && p->L == v) || (g->R == p && p->R == v);
-            if (zigzig) {
+        else {
+            int g = t[p].P;
+            if((t[g].L == p && t[p].L == v) || (t[g].R == p && t[p].R == v))
                 zig_zig(v);
-            } else {
+            else
                 zig_zag(v);
-            }
         }
     }
 }
 
-SplayTree* get_kth(SplayTree*& v, int k) {
-    if (!v) {
-        return 0;
-    }
-    // as normal BST, but use splay before return
-    while (true) {
-        push(v);
-        if (get_size(v->L) + 1 == k) {
-            if (v->L) {
-                push(v->L);
-            }
-            splay(v);
-            return v;
-        }
-        if (k <= get_size(v->L)) {
-            v = v->L;
-        } else {
-            k -= get_size(v->L) + 1;
-            v = v->R;
-        }
-    }
+int get_kth(int v, int k) {
+    push(v);
+    int left_size = get_size(t[v].L);
+    if(left_size + 1 == k) {
+        if(t[v].L != -1) push(t[v].L);
+        splay(v);
+        return v;
+    } else if(k <= left_size)
+        return get_kth(t[v].L, k);
+    else
+        return get_kth(t[v].R, k - left_size - 1);
 }
 
-SplayTree* merge(SplayTree* L, SplayTree* R) {
-    if (!L) return R;
-    if (!R) return L;
-
-    while (L->R) {
-        push(L);
-        L = L->R;
-    }
+int merge(int L, int R) {
+    if(L == -1) return R;
+    if(R == -1) return L;
     push(L);
+    while(t[L].R != -1) {
+        push(L);
+        L = t[L].R;
+        push(L);
+    }
     splay(L);
-    L->R = R;
-    R->P = L;
+    t[L].R = R;
+    t[R].P = L;
     update(L);
     return L;
 }
 
-// return [L, R], where sz(L) == k, so it means we get first k elements
-pair<SplayTree*, SplayTree*> split(SplayTree* v, int x) {
-    if (!v) return {0, 0};
-    if (x <= 0) return {nullptr, v};
-    if (x >= get_size(v)) return {v, nullptr};
-    // v is k_th
-    // so it has k - 1 in left
+pair<int, int> split(int v, int x) {
+    if(v == -1) return {-1, -1};
+    if(x <= 0) return {-1, v};
+    if(x >= get_size(v)) return {v, -1};
     v = get_kth(v, x);
-    SplayTree* R = v->R;
-    set_parent(R, 0);
-    v->R = 0;
+    int R = t[v].R;
+    set_parent(R, -1);
+    t[v].R = -1;
     update(v);
     return {v, R};
 }
 
-void push_back(SplayTree*& v, int val) {
-    SplayTree* temp = new SplayTree(val);
+void push_back(int &v, int val) {
+    int temp = new_node(val);
     v = merge(v, temp);
 }
 
-void reverse(SplayTree*& v, int l, int r) {
-    auto [M, R] = split(v, r + 1);
-    auto [L, T] = split(M, l);
-    T->is_rev = true;
-    v = merge(merge(L, T), R);
+void reverse_interval(int &v, int l, int r) {
+    auto AB = split(v, l);
+    int A = AB.first, B = AB.second;
+    auto CD = split(B, r - l + 1);
+    int C = CD.first, D = CD.second;
+    if(C != -1) t[C].is_rev ^= 1;
+    v = merge(merge(A, C), D);
 }
 
-int get_min(SplayTree*& v, int l, int r) {
-    auto [M, R] = split(v, r + 1);
-    auto [L, T] = split(M, l);
-    int res = get_min(T);
-    v = merge(merge(L, T), R);
+int query_min(int &v, int l, int r) {
+    auto AB = split(v, l);
+    int A = AB.first, B = AB.second;
+    auto CD = split(B, r - l + 1);
+    int C = CD.first, D = CD.second;
+    int res = get_min_val(C);
+    v = merge(merge(A, C), D);
     return res;
 }
 
 void solve() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
     int n, m;
     cin >> n >> m;
-
-    SplayTree* root = 0;
-
+    int root = -1;
     for (int i = 0; i < n; i++) {
         int x;
         cin >> x;
         push_back(root, x);
     }
-    for (int i = 0; i < m; i++) {
+    while(m--) {
         int type, l, r;
         cin >> type >> l >> r;
-        l--;
-        r--;
-        if (type == 1)
-            reverse(root, l, r);
+        l--; r--;
+        if(type == 1)
+            reverse_interval(root, l, r);
         else
-            cout << get_min(root, l, r) << '\n';
+            cout << query_min(root, l, r) << "\n";
     }
 }
 
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+int main(){
     solve();
+    return 0;
 }
